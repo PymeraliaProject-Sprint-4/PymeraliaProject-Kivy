bbeeca0b555ee118744b62f07fc59b66c729aca9
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2023-04-12 16:06:44 +0200

    Nova taula

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ conn = sqlite3.connect('pymeshield.db')
 cursor = conn.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT NOT NULL, recommendation TEXT NOT NULL, danger TEXT NOT NULL, manages TEXT NOT NULL, price FLOAT NULL, price_customer FLOAT NOT NULL)')
 cursor.execute('CREATE TABLE IF NOT EXISTS budgets (id INTEGER PRIMARY KEY, name TEXT NOT NULL, recommendation TEXT NOT NULL, danger TEXT NOT NULL, manages TEXT NOT NULL, price FLOAT NULL, price_customer FLOAT NOT NULL)')
+cursor.execute('CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY, name TEXT NOT NULL, status TEXT NULL, date DATE NULL)')
 conn.commit()
 conn.close()
             
