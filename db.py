189640430ca225bcd63c9110e44a38765d451d64
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2023-04-13 15:54:02 +0200

    Updated db.py
    afegida taula reports a la bbdd

--- db.py
+++ db.py
@@ -1,14 +1,12 @@
-import json  # importamos la libreria de python que nos permite trabajar con json
-from pathlib import Path  # cargar ruta del script
-import requests
 import sqlite3
-import threading
-import time
+
 conn = sqlite3.connect('pymeshield.db')
 
 cursor = conn.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT NOT NULL, recommendation TEXT NOT NULL, danger TEXT NOT NULL, manages TEXT NOT NULL, price FLOAT NULL, price_customer FLOAT NOT NULL)')
-# cursor.execute('CREATE TABLE IF NOT EXISTS budgets (id INTEGER PRIMARY KEY, name TEXT NOT NULL, recommendation TEXT NOT NULL, danger TEXT NOT NULL, manages TEXT NOT NULL, price FLOAT NULL, price_customer FLOAT NOT NULL)')
+cursor.execute('CREATE TABLE IF NOT EXISTS budgets (id INTEGER PRIMARY KEY, price FLOAT NULL, accepted TEXT NOT NULL)')
+cursor.execute('CREATE TABLE IF NOT EXISTS reports (id INTEGER PRIMARY KEY, name TEXT NOT NULL, status TEXT NULL, date DATE NULL)')
+
 conn.commit()
 conn.close()
             
